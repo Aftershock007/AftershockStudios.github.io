58516c4d43b9446e23016ec3fad43d6d0fac9531
Arijit <biswasarijit8017652954@gmail.com>
2021-01-25 20:31:16 +0530

    Update

--- Bike-boy/app.js
+++ Bike-boy/app.js
@@ -1,23 +1,23 @@
-const express = require("express");
-const path = require("path");
-const mongoose = require("mongoose");
-require("dotenv/config")
-const app = express();
-const port = 80;
+// const express = require("express");
+// const path = require("path");
+// const mongoose = require("mongoose");
+// require("dotenv/config")
+// const app = express();
+// const port = 80;
 
-app.use(express.json());
-app.get("/", (req, res) => {
-    res.send("Hello World!!");
-});
+// app.use(express.json());
+// app.get("/", (req, res) => {
+//     res.send("Hello World!!");
+// });
 
-mongoose.connect(process.env.DB_CONNECTION, {
-        useUnifiedTopology: true,
-        useNewUrlParser: true
-    },
-    (req, res) => {
-        console.log("Connected");
-    });
+// mongoose.connect(process.env.DB_CONNECTION, {
+//         useUnifiedTopology: true,
+//         useNewUrlParser: true
+//     },
+//     (req, res) => {
+//         console.log("Connected");
+//     });
 
-app.listen(port, () => {
-    console.log(`The application started successfully on port ${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`The application started successfully on port ${port}`);
+// });
