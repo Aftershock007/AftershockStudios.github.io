2c54f69339c6e417c9e60cef66ae543b637a3f8d
Arijit <biswasarijit8017652954@gmail.com>
2021-01-25 20:21:42 +0530

    Aftershock Studios
    This website is created with love by Arijit Biswas.

---
+++ Bike-boy/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+require("dotenv/config")
+const app = express();
+const port = 80;
+
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("Hello World!!");
+});
+
+mongoose.connect(process.env.DB_CONNECTION, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    },
+    (req, res) => {
+        console.log("Connected");
+    });
+
+app.listen(port, () => {
+    console.log(`The application started successfully on port ${port}`);
+});
